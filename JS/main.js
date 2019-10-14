b0f02796a28d710576ce9a1ab8a7815cf17bf4a7
Thomas Kahr <Kahr@mathe.inet>
2019-10-14 14:39:12 +0200

    status 14102019 mittag

--- JS/main.js
+++ JS/main.js
@@ -1,64 +1,75 @@
 /* Change Color on Scroll Different Functions */
 
 $(function() {
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 270) {
-         $('.wrapper').addClass('changeColorWhite')
-      }
-      if ($(this).scrollTop() < 270) {
-         $('.wrapper').removeClass('changeColorWhite')
-      }
-      if ($(this).scrollTop() > 3000) {
-         $('.wrapper').removeClass('changeColorWhite')
-      }
-   });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 270) {
+      $(".wrapper").addClass("changeColorDark");
+    }
+    if ($(this).scrollTop() < 270) {
+      $(".wrapper").removeClass("changeColorDark");
+    }
+    if ($(this).scrollTop() > 1500) {
+      $(".wrapper").removeClass("changeColorDark");
+    }
+  });
 });
 $(function() {
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 3000) {
-         $('.wrapper').addClass('changeColorLight')
-      }
-      if ($(this).scrollTop() < 3000) {
-         $('.wrapper').removeClass('changeColorLight')
-      }
-   });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1500) {
+      $(".wrapper").addClass("changeColorLight");
+    }
+    if ($(this).scrollTop() < 1500) {
+      $(".wrapper").removeClass("changeColorLight");
+    }
+  });
 });
 $(function() {
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 1900) {
-         $('body').addClass('changeColorBlue')
-      }
-      if ($(this).scrollTop() < 1900) {
-         $('body').removeClass('changeColorBlue')
-      }
-   });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1500) {
+      $("body").addClass("changeColorBlue");
+    }
+    if ($(this).scrollTop() < 1500) {
+      $("body").removeClass("changeColorBlue");
+    }
+    if ($(this).scrollTop() > 2800) {
+      $("body").addClass("changeColorRed");
+    }
+    if ($(this).scrollTop() < 2800) {
+      $("body").removeClass("changeColorRed");
+    }
+  });
 });
 /* END Change Color on Scroll Different Functions */
 
 /* Menu Toggle and other .active Functions */
-$(document).ready(function(){
-   $('.menu-toggle').click(function(){
-     $('.menu-toggle').toggleClass('active')
-     $('.main-nav').toggleClass('active')
-     $('.wrapper').toggleClass('active')
-     $('.social').toggleClass('active')
-     $('h1').toggleClass('active')
-   })
-   })
+$(document).ready(function() {
+  var ToggleActive = 0;
+
+  $(".menu-toggle").click(function openclose() {
+    if (ToggleActive != 1) {
+      $(".menu-toggle").toggleClass("active");
+      $(".main-nav").toggleClass("active");
+      $(".wrapper").toggleClass("active");
+      $(".social").toggleClass("active");
+      $("h1").toggleClass("active");
+      ToggleActive = 1;
+    } else {
+      $(".menu-toggle").toggleClass("active", false);
+      $(".main-nav").toggleClass("active", false);
+      $(".wrapper").toggleClass("active", false);
+      $(".social").toggleClass("active", false);
+      $("h1").toggleClass("active", false);
+      ToggleActive = 0;
+    }
+  });
+  $(".main-nav").click(function close() {
+    $(".menu-toggle").toggleClass("active", false);
+    $(".main-nav").toggleClass("active", false);
+    $(".wrapper").toggleClass("active", false);
+    $(".social").toggleClass("active", false);
+    $("h1").toggleClass("active", false);
+    ToggleActive = 0;
+  });
+});
+
 /* END Menu Toggle and other .active Functions */
-
-/* Parallax Products */
-function splitScroll() {
-   const controller = new ScrollMagic.Controller();
-
-   new ScrollMagic.Scene({
-       duration: '185%',
-       triggerElement: '.product-title',
-       triggerHook: 0.1
-   })
-   .setPin('.product-title')
-   .addTo(controller);
-}
-
-splitScroll();
-/* END Parallax Products */
